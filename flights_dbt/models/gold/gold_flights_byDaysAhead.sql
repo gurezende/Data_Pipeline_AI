48ecf9f5a7837ba7fa181d1fab7cde0d06136aca
Gustavo Santos <gurezendes@gmail.com>
2025-03-04 08:37:21 -0500

    Fix: Added gold table price by days ahead and generate docs again

---
+++ flights_dbt/models/gold/gold_flights_byDaysAhead.sql
@@ -0,0 +1,14 @@
+-- gold layer from flights DB
+
+{{ config(materialized='view') }}
+
+-- Average price by depart_city and days_ahead
+select
+	depart_city,
+	days_before_flight ,
+	ROUND( cast( AVG(ticket_prices_brl) as NUMERIC ), 3) as average_ticket_price
+from {{ ref('silver_flights') }}
+group by depart_city, days_before_flight
+order by depart_city, average_ticket_price asc
+    
+ 
